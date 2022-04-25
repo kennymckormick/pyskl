9e5cfdaab3639824806e40435e8a31241f07acc4
Haodong Duan <dhd.efz@gmail.com>
2022-04-25 22:04:06 +0800

    update

--- pyskl/models/gcns/__init__.py
+++ pyskl/models/gcns/__init__.py
@@ -1,6 +1,7 @@
 from .aagcn import AAGCN
+from .ctrgcn import CTRGCN
 from .msg3d import MSG3D
 from .stgcn import STGCN
 from .utils import mstcn, unit_aagcn, unit_gcn, unit_tcn
 
-__all__ = ['unit_gcn', 'unit_aagcn', 'unit_tcn', 'mstcn', 'STGCN', 'AAGCN', 'MSG3D']
+__all__ = ['unit_gcn', 'unit_aagcn', 'unit_tcn', 'mstcn', 'STGCN', 'AAGCN', 'MSG3D', 'CTRGCN']
