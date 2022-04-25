b362a6d25a75b292be2efb7574224300c246a1e5
Haodong Duan <dhd.efz@gmail.com>
2022-04-25 20:52:25 +0800

    reorg msg3d

--- pyskl/models/gcns/utils/__init__.py
+++ pyskl/models/gcns/utils/__init__.py
@@ -1,7 +1,9 @@
 from .gcn import unit_gcn
 from .init_func import bn_init, conv_branch_init, conv_init
+from .msg3d_utils import MSGCN, MSTCN, MW_MSG3DBlock
 from .tcn import mstcn, unit_tcn
 
 __all__ = [
-    'unit_gcn', 'unit_tcn', 'mstcn', 'conv_init', 'bn_init', 'conv_branch_init'
+    'unit_gcn', 'unit_tcn', 'mstcn', 'conv_init', 'bn_init', 'conv_branch_init',
+    'MSGCN', 'MSTCN', 'MW_MSG3DBlock'
 ]
