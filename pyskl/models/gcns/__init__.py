b208f480da142d63200042362fc6af0f4f98430d
Haodong Duan <dhd.efz@gmail.com>
2022-05-02 13:41:07 +0800

    Support SGN

--- pyskl/models/gcns/__init__.py
+++ pyskl/models/gcns/__init__.py
@@ -1,7 +1,8 @@
 from .aagcn import AAGCN
 from .ctrgcn import CTRGCN
 from .msg3d import MSG3D
+from .sgn import SGN
 from .stgcn import STGCN
 from .utils import mstcn, unit_aagcn, unit_gcn, unit_tcn
 
-__all__ = ['unit_gcn', 'unit_aagcn', 'unit_tcn', 'mstcn', 'STGCN', 'AAGCN', 'MSG3D', 'CTRGCN']
+__all__ = ['unit_gcn', 'unit_aagcn', 'unit_tcn', 'mstcn', 'STGCN', 'AAGCN', 'MSG3D', 'CTRGCN', 'SGN']
