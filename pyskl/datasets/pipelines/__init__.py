21fdaae1b21930fb994ec0591dc4a007e613969a
Haodong Duan <dhd.efz@gmail.com>
2022-05-09 16:00:23 +0800

    support PoTion

--- pyskl/datasets/pipelines/__init__.py
+++ pyskl/datasets/pipelines/__init__.py
@@ -2,7 +2,7 @@
 from .augmentations import *  # noqa: F401, F403
 from .compose import Compose  # noqa: F401, F403
 from .formatting import *  # noqa: F401, F403
+from .heatmap_related import *  # noqa: F401, F403
 from .loading import *  # noqa: F401, F403
 from .pose_related import *  # noqa: F401, F403
-from .posec3d_related import *  # noqa: F401, F403
 from .sampling import *  # noqa: F401, F403
