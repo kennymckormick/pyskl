18d2775e7c5213f1cc3096205ef5161fd842b470
kennymckormick <dhd@pku.edu.cn>
2022-05-25 15:55:58 +0800

    update smp

--- pyskl/smp.py
+++ pyskl/smp.py
@@ -127,3 +127,11 @@ def gen_bash(cfgs, num_gpus, gpus_per_task=1):
             )
         timestamp = time.strftime('%m%d%H%M%S', time.localtime())
         mwlines(cmds, f'train_{timestamp}_{i}.sh')
+
+def h2r(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+def r2h(rgb):
+    return '#%02x%02x%02x' % rgb
