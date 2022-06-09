5a2affea5e68cb307ab6c8e7d72b0477dac01231
Haodong Duan <dhd.efz@gmail.com>
2022-06-09 19:15:18 +0800

    Update hrnet_w32_coco_256x192.py

--- demo/hrnet_w32_coco_256x192.py
+++ demo/hrnet_w32_coco_256x192.py
@@ -130,5 +130,5 @@ data = dict(
         ann_file=f'{data_root}/annotations/person_keypoints_val2017.json',
         img_prefix=f'{data_root}/val2017/',
         data_cfg=data_cfg,
-        pipeline=val_pipeline),
+        pipeline=test_pipeline),
 )
