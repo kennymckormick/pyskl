9286c949d822be7199d4b534bc9a73bcc2de8749
kennymckormick <dhd@pku.edu.cn>
2022-12-12 19:53:59 +0800

    support dgstgcn

--- pyskl/models/gcns/__init__.py
+++ pyskl/models/gcns/__init__.py
@@ -1,8 +1,12 @@
 from .aagcn import AAGCN
 from .ctrgcn import CTRGCN
+from .dgstgcn import DGSTGCN
 from .msg3d import MSG3D
 from .sgn import SGN
 from .stgcn import STGCN
-from .utils import mstcn, unit_aagcn, unit_gcn, unit_tcn
+from .utils import mstcn, unit_aagcn, unit_gcn, unit_group_gcn, unit_tcn
 
-__all__ = ['unit_gcn', 'unit_aagcn', 'unit_tcn', 'mstcn', 'STGCN', 'AAGCN', 'MSG3D', 'CTRGCN', 'SGN']
+__all__ = [
+    'unit_gcn', 'unit_aagcn', 'unit_group_gcn', 'unit_tcn', 'mstcn',
+    'STGCN', 'AAGCN', 'MSG3D', 'CTRGCN', 'SGN', 'DGSTGCN'
+]
--- pyskl/models/gcns/utils/__init__.py
+++ pyskl/models/gcns/utils/__init__.py
@@ -1,11 +1,11 @@
-from .gcn import unit_aagcn, unit_ctrgcn, unit_gcn, unit_sgn
+from .gcn import unit_aagcn, unit_ctrgcn, unit_gcn, unit_group_gcn, unit_sgn
 from .init_func import bn_init, conv_branch_init, conv_init
 from .msg3d_utils import MSGCN, MSTCN, MW_MSG3DBlock
 from .tcn import mstcn, unit_tcn
 
 __all__ = [
     # GCN Modules
-    'unit_gcn', 'unit_aagcn', 'unit_ctrgcn', 'unit_sgn',
+    'unit_gcn', 'unit_aagcn', 'unit_ctrgcn', 'unit_group_gcn', 'unit_sgn',
     # TCN Modules
     'unit_tcn', 'mstcn',
     # MSG3D Utils
