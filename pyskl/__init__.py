a7c9fd04f2b8ae3ac3e0002f0db9aec98b7e429b
kennymckormick <dhd@pku.edu.cn>
2022-12-21 13:25:41 +0800

    update mmcv version

--- pyskl/__init__.py
+++ pyskl/__init__.py
@@ -5,7 +5,7 @@ from mmcv import digit_version
 from .version import __version__
 
 mmcv_minimum_version = '1.3.6'
-mmcv_maximum_version = '2.0.0'
+mmcv_maximum_version = '2.1.0'
 mmcv_version = digit_version(mmcv.__version__)
 
 assert (digit_version(mmcv_minimum_version) <= mmcv_version
