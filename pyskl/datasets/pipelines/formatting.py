afe99c8bd11d489552cd861fefc90d62904b3174
kennymckormick <dhd@pku.edu.cn>
2022-12-27 12:02:38 +0800

    fix

--- pyskl/datasets/pipelines/formatting.py
+++ pyskl/datasets/pipelines/formatting.py
@@ -226,18 +226,20 @@ class FormatShape(object):
                 results['imgs'] = imgs
                 results['input_shape'] = imgs.shape
         elif self.input_format == 'NCTHW_Heatmap':
-            num_clips = results['num_clips']
-            clip_len = results['clip_len']
+            if 'imgs' in results:
+                num_clips = results['num_clips']
+                clip_len = results['clip_len']
+                imgs = results['imgs']
 
-            imgs = imgs.reshape((-1, num_clips, clip_len) + imgs.shape[1:])
-            # N_crops x N_clips x L x C x H x W
-            imgs = np.transpose(imgs, (0, 1, 3, 2, 4, 5))
-            # N_crops x N_clips x C x L x H x W
-            imgs = imgs.reshape((-1, ) + imgs.shape[2:])
-            # M' x C x L x H x W
-            # M' = N_crops x N_clips
-            results['imgs'] = imgs
-            results['input_shape'] = imgs.shape
+                imgs = imgs.reshape((-1, num_clips, clip_len) + imgs.shape[1:])
+                # N_crops x N_clips x L x C x H x W
+                imgs = np.transpose(imgs, (0, 1, 3, 2, 4, 5))
+                # N_crops x N_clips x C x L x H x W
+                imgs = imgs.reshape((-1, ) + imgs.shape[2:])
+                # M' x C x L x H x W
+                # M' = N_crops x N_clips
+                results['imgs'] = imgs
+                results['input_shape'] = imgs.shape
 
         return results
 
