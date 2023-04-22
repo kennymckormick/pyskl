30aa2af0f23620b5e1adb9d71397df39de519c9f
anhvu2501 <vu25ck28@gmail.com>
2023-04-23 00:55:35 +0700

    change command to run on Kaggle

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -7,6 +7,6 @@ CONFIG=$1
 GPUS=$2
 
 MKL_SERVICE_FORCE_INTEL=1 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$MASTER_PORT \
+/opt/conda/envs/pyskl/bin/python3 -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$MASTER_PORT \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
 # Any arguments from the third one are captured by ${@:3}
