c3d2f3f9be239d743795085877f065f0e88d536d
anhvu2501 <vu25ck28@gmail.com>
2023-04-24 11:41:47 +0700

    change to eval checkpoints

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -9,5 +9,5 @@ GPUS=$3
 
 MKL_SERVICE_FORCE_INTEL=1 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 # Arguments starting from the forth one are captured by ${@:4}
-python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$MASTER_PORT \
+/opt/conda/envs/pyskl/bin/python3 -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$MASTER_PORT \
     $(dirname "$0")/test.py $CONFIG -C $CHECKPOINT --launcher pytorch ${@:4}
