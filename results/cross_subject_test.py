67dc479597ef30431ae01ee1dd178d101f9d381d
anhvu2501 <vu25ck28@gmail.com>
2023-05-14 11:26:05 +0700

    CS and CV experiments updated, modify bone d2 (target -> source)

--- results/cross_subject_test.py
+++ results/cross_subject_test.py
@@ -61,37 +61,19 @@ for annotation in range(len(data[0]['annotations'])):
 print(len(label))
 # print(label)
 
-joint_out = []
+# Read pkl file with pickle
 with (open("stgcn/stgcn_pyskl_ntu60_xsub_hrnet/j_result.pkl", "rb")) as openfile:
-    while True:
-        try:
-            joint_out.append(pickle.load(openfile))
-        except EOFError:
-            break
+    joint_out = pickle.load(openfile)
 
-bone_out = []
 with (open("stgcn/stgcn_pyskl_ntu60_xsub_hrnet/b_result.pkl", "rb")) as openfile:
-    while True:
-        try:
-            bone_out.append(pickle.load(openfile))
-        except EOFError:
-            break
+    bone_out = pickle.load(openfile)
 
 jm_out = []
 with (open("stgcn/stgcn_pyskl_ntu60_xsub_hrnet/jm_result.pkl", "rb")) as openfile:
-    while True:
-        try:
-            jm_out.append(pickle.load(openfile))
-        except EOFError:
-            break
+    jm_out = pickle.load(openfile)
 
-bm_out = []
 with (open("stgcn/stgcn_pyskl_ntu60_xsub_hrnet/bm_result.pkl", "rb")) as openfile:
-    while True:
-        try:
-            bm_out.append(pickle.load(openfile))
-        except EOFError:
-            break
+    bm_out = pickle.load(openfile)
 
 print("******")
 print(len(joint_out))
@@ -99,6 +81,7 @@ print("******")
 # print(joint_out[0][0])
 # print(joint_out[1][0])
 
+# Read pkl file with pandas
 joint_out_pd = pd.read_pickle("stgcn/stgcn_pyskl_ntu60_xsub_hrnet/j_result.pkl")
 bone_out_pd = pd.read_pickle("stgcn/stgcn_pyskl_ntu60_xsub_hrnet/b_result.pkl")
 jm_out_pd = pd.read_pickle("stgcn/stgcn_pyskl_ntu60_xsub_hrnet/jm_result.pkl")
@@ -107,23 +90,10 @@ bm_out_pd = pd.read_pickle("stgcn/stgcn_pyskl_ntu60_xsub_hrnet/bm_result.pkl")
 # print(len(joint_out_pd))
 # print(joint_out_pd[0].shape)
 # print(joint_out_pd[0])
-
-action_scores_4s = []
-for i in range(len(joint_out_pd)):
-    action_scores_4s.append(2 * joint_out_pd[i] + 2 * bone_out_pd[i] + jm_out_pd[i] + bm_out_pd[i])
-
-print(len(action_scores_4s))
-
-# action_scores = 2 * joint_out + 2 * bone_out + jm_out + bm_out
-
-# print(len(action_scores))
-action_label_4s = []
-for i in range(len(action_scores_4s)):
-    action_label_4s.append(np.argmax(action_scores_4s[i]))
-
-print(len(action_label_4s))
 # print(type(action_label_4s))
 
+### Cross Subject Two stream experiments - (Joint : Bone)
+# Two stream: Joint : Bone = 1 : 1
 action_scores_2s = []
 for i in range(len(joint_out_pd)):
     action_scores_2s.append(joint_out_pd[i] + bone_out_pd[i])
@@ -132,54 +102,225 @@ action_label_2s = []
 for i in range(len(action_scores_2s)):
     action_label_2s.append(np.argmax(action_scores_2s[i]))
 
-# for i in range(10):
-#     print(action_label[i+10])
-#     print(label[i+10])
-# top1_result = accuracy(action_label, label)
-# print(top1_result)
-
-print("Four-stream accuracy Joint : Bone : JM : BM = 2 : 2 : 1 : 1")
-accuracy_score_4s = sum(1 for x,y in zip(action_label_4s, label) if x == y) / float(len(action_label_4s))
-print(accuracy_score_4s)
-
 print("Two-stream accuracy Joint : Bone = 1 : 1")
 accuracy_score_2s = sum(1 for x,y in zip(action_label_2s, label) if x == y) / float(len(action_label_2s))
 print(accuracy_score_2s)
 
-
-action_scores_2s_weight1 = []
-for i in range(len(joint_out_pd)):
-    action_scores_2s_weight1.append(joint_out_pd[i] + 2 * bone_out_pd[i])
-
-action_label_2s_weight1 = []
-for i in range(len(action_scores_2s_weight1)):
-    action_label_2s_weight1.append(np.argmax(action_scores_2s_weight1[i]))
-
-accuracy_score_2s_weight1 = sum(1 for x,y in zip(action_label_2s_weight1, label) if x == y) / float(len(action_label_2s_weight1))
-print("Two stream accuracy Joint : Bone = 1 : 2")
-print(accuracy_score_2s_weight1)
-
+# Two stream: Joint : Bone = 1 : 2
 action_scores_2s_weight2 = []
 for i in range(len(joint_out_pd)):
-    action_scores_2s_weight2.append(joint_out_pd[i] + 3 * bone_out_pd[i])
+    action_scores_2s_weight2.append(joint_out_pd[i] + 2 * bone_out_pd[i])
 
 action_label_2s_weight2 = []
 for i in range(len(action_scores_2s_weight2)):
     action_label_2s_weight2.append(np.argmax(action_scores_2s_weight2[i]))
 
 accuracy_score_2s_weight2 = sum(1 for x,y in zip(action_label_2s_weight2, label) if x == y) / float(len(action_label_2s_weight2))
-print("Two stream accuracy Joint : Bone = 1 : 3")
+print("Two stream accuracy Joint : Bone = 1 : 2")
 print(accuracy_score_2s_weight2)
 
-# action_scores_2s_weight3 = []
-# for i in range(len(joint_out_pd)):
-#     action_scores_2s_weight3.append(joint_out_pd[i] + 4 * bone_out_pd[i])
+# Two stream: Joint : Bone = 1 : 3
+action_scores_2s_weight3 = []
+for i in range(len(joint_out_pd)):
+    action_scores_2s_weight3.append(joint_out_pd[i] + 3 * bone_out_pd[i])
 
-# action_label_2s_weight3 = []
-# for i in range(len(action_scores_2s_weight3)):
-#     action_label_2s_weight1.append(np.argmax(action_scores_2s_weight3[i]))
+action_label_2s_weight3 = []
+for i in range(len(action_scores_2s_weight3)):
+    action_label_2s_weight3.append(np.argmax(action_scores_2s_weight3[i]))
 
-# accuracy_score_2s_weight3 = sum(1 for x,y in zip(action_label_2s_weight3, label) if x == y) / float(len(action_label_2s_weight3))
-# print("Two stream accuracy Joint : Bone = 1 : 4")
-# print(accuracy_score_2s_weight3)
+accuracy_score_2s_weight3 = sum(1 for x,y in zip(action_label_2s_weight3, label) if x == y) / float(len(action_label_2s_weight3))
+print("Two stream accuracy Joint : Bone = 1 : 3")
+print(accuracy_score_2s_weight3)
+
+# Two stream: Joint : Bone = 1 : 4
+action_scores_2s_weight4 = []
+for i in range(len(joint_out_pd)):
+    action_scores_2s_weight4.append(joint_out_pd[i] + 4 * bone_out_pd[i])
+
+action_label_2s_weight4 = []
+for i in range(len(action_scores_2s_weight4)):
+    action_label_2s_weight4.append(np.argmax(action_scores_2s_weight4[i]))
+
+accuracy_score_2s_weight4 = sum(1 for x,y in zip(action_label_2s_weight4, label) if x == y) / float(len(action_label_2s_weight4))
+print("Two stream accuracy Joint : Bone = 1 : 4")
+print(accuracy_score_2s_weight4)
+
+
+### Cross Subject benchmark: Two stream experiments - (Joint, Joint Motion)
+action_scores_jjm_2s = []
+for i in range(len(joint_out_pd)):
+    action_scores_jjm_2s.append(joint_out_pd[i] + jm_out_pd[i])
+
+action_label_jjm_2s = []
+for i in range(len(action_scores_jjm_2s)):
+    action_label_jjm_2s.append(np.argmax(action_scores_jjm_2s[i]))
+
+print("Two-stream accuracy Joint : Joint Motion = 1 : 1")
+accuracy_score_jjm_2s = sum(1 for x,y in zip(action_label_jjm_2s, label) if x == y) / float(len(action_label_jjm_2s))
+print(accuracy_score_jjm_2s)
+
+
+### Cross Subject benchmark: Two stream experiments - (Joint, Bone Motion)
+action_scores_jbm_2s = []
+for i in range(len(joint_out_pd)):
+    action_scores_jbm_2s.append(joint_out_pd[i] + bm_out_pd[i])
+
+action_label_jbm_2s = []
+for i in range(len(action_scores_jbm_2s)):
+    action_label_jbm_2s.append(np.argmax(action_scores_jbm_2s[i]))
+
+print("Two-stream accuracy Joint : Bone Motion = 1 : 1")
+accuracy_score_jbm_2s = sum(1 for x,y in zip(action_label_jbm_2s, label) if x == y) / float(len(action_label_jbm_2s))
+print(accuracy_score_jbm_2s)
+
+
+### Cross Subject benchmark: Two stream experiments - (Bone, Joint Motion)
+action_scores_bjm_2s = []
+for i in range(len(joint_out_pd)):
+    action_scores_bjm_2s.append(bone_out_pd[i] + jm_out_pd[i])
+
+action_label_bjm_2s = []
+for i in range(len(action_scores_bjm_2s)):
+    action_label_bjm_2s.append(np.argmax(action_scores_bjm_2s[i]))
+
+print("Two-stream accuracy Bone : Joint Motion = 1 : 1")
+accuracy_score_bjm_2s = sum(1 for x,y in zip(action_label_bjm_2s, label) if x == y) / float(len(action_label_bjm_2s))
+print(accuracy_score_bjm_2s)
+
+
+### Cross Subject benchmark: Two stream experiments - (Bone, Bone Motion)
+action_scores_bbm_2s = []
+for i in range(len(joint_out_pd)):
+    action_scores_bbm_2s.append(bone_out_pd[i] + bm_out_pd[i])
+
+action_label_bbm_2s = []
+for i in range(len(action_scores_bbm_2s)):
+    action_label_bbm_2s.append(np.argmax(action_scores_bbm_2s[i]))
+
+print("Two-stream accuracy Bone : Bone Motion = 1 : 1")
+accuracy_score_bbm_2s = sum(1 for x,y in zip(action_label_bbm_2s, label) if x == y) / float(len(action_label_bbm_2s))
+print(accuracy_score_bbm_2s)
+
+
+### Cross Subject benchmark: Two stream experiments - (Joint Motion, Bone Motion)
+action_scores_jmbm_2s = []
+for i in range(len(joint_out_pd)):
+    action_scores_jmbm_2s.append(jm_out_pd[i] + bm_out_pd[i])
+
+action_label_jmbm_2s = []
+for i in range(len(action_scores_jmbm_2s)):
+    action_label_jmbm_2s.append(np.argmax(action_scores_jmbm_2s[i]))
+
+print("Two-stream accuracy Joint Motion : Bone Motion = 1 : 1")
+accuracy_score_jmbm_2s = sum(1 for x,y in zip(action_label_jmbm_2s, label) if x == y) / float(len(action_label_jmbm_2s))
+print(accuracy_score_jmbm_2s)
+
+
+### Cross Subject benchmark: Three stream experiments
+# Three stream: Joint : Bone : Joint Motion
+action_scores_3s_jbjm = []
+for i in range(len(joint_out_pd)):
+    action_scores_3s_jbjm.append(joint_out_pd[i] + bone_out_pd[i] + jm_out_pd[i])
+
+action_label_3s_jbjm = []
+for i in range(len(action_scores_3s_jbjm)):
+    action_label_3s_jbjm.append(np.argmax(action_scores_3s_jbjm[i]))
+
+print("Three-stream accuracy Joint : Bone : JM = 1 : 1 : 1")
+accuracy_score_3s_jbjm = sum(1 for x,y in zip(action_label_3s_jbjm, label) if x == y) / float(len(action_label_3s_jbjm))
+print(accuracy_score_3s_jbjm)
+
+# Three stream: Joint : Bone : Bone Motion
+action_scores_3s_jbbm = []
+for i in range(len(joint_out_pd)):
+    action_scores_3s_jbbm.append(joint_out_pd[i] + bone_out_pd[i] + bm_out_pd[i])
+
+action_label_3s_jbbm = []
+for i in range(len(action_scores_3s_jbbm)):
+    action_label_3s_jbbm.append(np.argmax(action_scores_3s_jbbm[i]))
+
+print("Three-stream accuracy Joint : Bone : BM = 1 : 1 : 1")
+accuracy_score_3s_jbbm = sum(1 for x,y in zip(action_label_3s_jbbm, label) if x == y) / float(len(action_label_3s_jbbm))
+print(accuracy_score_3s_jbbm)
+
+
+### Cross Subject benchmark: Four stream weights experiments
+# Four stream: Joint : Bone : JM : BM = 1 : 1 : 1 : 1
+action_scores_4s = []
+for i in range(len(joint_out_pd)):
+    action_scores_4s.append(joint_out_pd[i] + bone_out_pd[i] + jm_out_pd[i] + bm_out_pd[i])
+
+action_label_4s = []
+for i in range(len(action_scores_4s)):
+    action_label_4s.append(np.argmax(action_scores_4s[i]))
+
+print("Four-stream accuracy Joint : Bone : JM : BM = 1 : 1 : 1 : 1")
+accuracy_score_4s = sum(1 for x,y in zip(action_label_4s, label) if x == y) / float(len(action_label_4s))
+print(accuracy_score_4s)
+
+# Four stream: Joint : Bone : JM : BM = 1 : 2 : 1 : 1
+action_scores_4s_12 = []
+for i in range(len(joint_out_pd)):
+    action_scores_4s_12.append(1 * joint_out_pd[i] + 2 * bone_out_pd[i] + jm_out_pd[i] + bm_out_pd[i])
+
+action_label_4s_12 = []
+for i in range(len(action_scores_4s_12)):
+    action_label_4s_12.append(np.argmax(action_scores_4s_12[i]))
+
+print("Four-stream accuracy Joint : Bone : JM : BM = 1 : 2 : 1 : 1")
+accuracy_score_4s_12 = sum(1 for x,y in zip(action_label_4s_12, label) if x == y) / float(len(action_label_4s_12))
+print(accuracy_score_4s_12)
+
+
+# Four stream: Joint : Bone : JM : BM = 1 : 3 : 1 : 1
+action_scores_4s_13 = []
+for i in range(len(joint_out_pd)):
+    action_scores_4s_13.append(1 * joint_out_pd[i] + 3 * bone_out_pd[i] + jm_out_pd[i] + bm_out_pd[i])
+
+action_label_4s_13 = []
+for i in range(len(action_scores_4s_13)):
+    action_label_4s_13.append(np.argmax(action_scores_4s_13[i]))
+
+print("Four-stream accuracy Joint : Bone : JM : BM = 1 : 3 : 1 : 1")
+accuracy_score_4s_13 = sum(1 for x,y in zip(action_label_4s_13, label) if x == y) / float(len(action_label_4s_13))
+print(accuracy_score_4s_13)
+
+# Four stream: Joint : Bone : JM : BM = 2 : 2 : 1 : 1
+action_scores_4s_2 = []
+for i in range(len(joint_out_pd)):
+    action_scores_4s_2.append(2 * joint_out_pd[i] + 2 * bone_out_pd[i] + jm_out_pd[i] + bm_out_pd[i])
+
+action_label_4s_2 = []
+for i in range(len(action_scores_4s_2)):
+    action_label_4s_2.append(np.argmax(action_scores_4s_2[i]))
+
+print("Four-stream accuracy Joint : Bone : JM : BM = 2 : 2 : 1 : 1")
+accuracy_score_4s_2 = sum(1 for x,y in zip(action_label_4s_2, label) if x == y) / float(len(action_label_4s_2))
+print(accuracy_score_4s_2)
+
+# Four stream: Joint : Bone : JM : BM = 2 : 3 : 1 : 1
+action_scores_4s_23 = []
+for i in range(len(joint_out_pd)):
+    action_scores_4s_23.append(2 * joint_out_pd[i] + 3 * bone_out_pd[i] + jm_out_pd[i] + bm_out_pd[i])
+
+action_label_4s_23 = []
+for i in range(len(action_scores_4s_23)):
+    action_label_4s_23.append(np.argmax(action_scores_4s_23[i]))
+
+print("Four-stream accuracy Joint : Bone : JM : BM = 2 : 3 : 1 : 1")
+accuracy_score_4s_23 = sum(1 for x,y in zip(action_label_4s_23, label) if x == y) / float(len(action_label_4s_23))
+print(accuracy_score_4s_23) 
+
+# Four stream: Joint : Bone : JM : BM = 2 : 4 : 1 : 1
+action_scores_4s_24 = []
+for i in range(len(joint_out_pd)):
+    action_scores_4s_24.append(2 * joint_out_pd[i] + 4 * bone_out_pd[i] + jm_out_pd[i] + bm_out_pd[i])
+
+action_label_4s_24 = []
+for i in range(len(action_scores_4s_24)):
+    action_label_4s_24.append(np.argmax(action_scores_4s_24[i]))
+
+print("Four-stream accuracy Joint : Bone : JM : BM = 2 : 4 : 1 : 1")
+accuracy_score_4s_24 = sum(1 for x,y in zip(action_label_4s_24, label) if x == y) / float(len(action_label_4s_24))
+print(accuracy_score_4s_24)
 
--- results/cross_view_test.py
+++ results/cross_view_test.py
@@ -50,34 +50,247 @@ with (open("stgcn/stgcn_pyskl_ntu60_xview_hrnet/j_result.pkl", "rb")) as openfil
 with (open("stgcn/stgcn_pyskl_ntu60_xview_hrnet/b_result.pkl", "rb")) as openfile:
     bone_out_cv = pickle.load(openfile)
 
+with (open("stgcn/stgcn_pyskl_ntu60_xview_hrnet/jm_result.pkl", "rb")) as openfile:
+    jm_out_cv = pickle.load(openfile)
 
-# Cross view benchmark two stream experiments
-action_scores_2s_cv = []
+with (open("stgcn/stgcn_pyskl_ntu60_xview_hrnet/bm_result.pkl", "rb")) as openfile:
+    bm_out_cv = pickle.load(openfile)
+
+
+### Cross view benchmark two stream experiments
+# Two stream: Joint : Bone = 1 : 1
+action_scores_2s = []
 for i in range(len(joint_out_cv)):
-    action_scores_2s_cv.append(joint_out_cv[i] + bone_out_cv[i])
+    action_scores_2s.append(joint_out_cv[i] + bone_out_cv[i])
 
+action_label_2s = []
+for i in range(len(action_scores_2s)):
+    action_label_2s.append(np.argmax(action_scores_2s[i]))
 
-action_label_2s_cv = []
-for i in range(len(action_scores_2s_cv)):
-    action_label_2s_cv.append(np.argmax(action_scores_2s_cv[i]))
+accuracy_score_2s = sum(1 for x, y in zip(action_label_2s, label) if x == y) / float(len(action_label_2s))
+print("Two stream accuraycy Joint : Bone = 1 : 1")
+print(accuracy_score_2s) 
 
-print(len(action_label_2s_cv))
-
-accuracy_score_2s_cv = sum(1 for x, y in zip(action_label_2s_cv, label) if x == y) / float(len(action_label_2s_cv))
-print("Cross View benchmark Two stream accuraycy Joint : Bone = 1 : 1")
-print(accuracy_score_2s_cv) 
-
-
-action_scores_2s_cv_2 = []
+# Two stream: Joint : Bone = 2 : 1
+action_scores_2s_2 = []
 for i in range(len(joint_out_cv)):
-    action_scores_2s_cv_2.append(2 * joint_out_cv[i] + bone_out_cv[i])
+    action_scores_2s_2.append(2 * joint_out_cv[i] + bone_out_cv[i])
 
-action_label_2s_cv_2 = []
-for i in range(len(action_scores_2s_cv_2)):
-    action_label_2s_cv_2.append(np.argmax(action_scores_2s_cv_2[i]))
+action_label_2s_2 = []
+for i in range(len(action_scores_2s_2)):
+    action_label_2s_2.append(np.argmax(action_scores_2s_2[i]))
 
-accuracy_score_2s_cv_2 = sum(1 for x, y in zip(action_label_2s_cv_2, label) if x == y) / float(len(action_label_2s_cv_2))
-print("Cross View benchmark Two stream accuraycy Joint : Bone = 2 : 1")
-print(accuracy_score_2s_cv_2) 
+accuracy_score_2s_2 = sum(1 for x, y in zip(action_label_2s_2, label) if x == y) / float(len(action_label_2s_2))
+print("Two stream accuraycy Joint : Bone = 2 : 1")
+print(accuracy_score_2s_2) 
+
+# Two stream: Joint : Bone = 3 : 1
+action_scores_2s_3 = []
+for i in range(len(joint_out_cv)):
+    action_scores_2s_3.append(3 * joint_out_cv[i] + bone_out_cv[i])
+
+action_label_2s_3 = []
+for i in range(len(action_scores_2s_3)):
+    action_label_2s_3.append(np.argmax(action_scores_2s_3[i]))
+
+accuracy_score_2s_3 = sum(1 for x, y in zip(action_label_2s_3, label) if x == y) / float(len(action_label_2s_3))
+print("Two stream accuraycy Joint : Bone = 3 : 1")
+print(accuracy_score_2s_3) 
+
+# Two stream: Joint : Bone = 4 : 1
+action_scores_2s_4 = []
+for i in range(len(joint_out_cv)):
+    action_scores_2s_4.append(4 * joint_out_cv[i] + bone_out_cv[i])
+
+action_label_2s_4 = []
+for i in range(len(action_scores_2s_4)):
+    action_label_2s_4.append(np.argmax(action_scores_2s_4[i]))
+
+accuracy_score_2s_4 = sum(1 for x, y in zip(action_label_2s_4, label) if x == y) / float(len(action_label_2s_4))
+print("Two stream accuraycy Joint : Bone = 4 : 1")
+print(accuracy_score_2s_4) 
+
+# Two stream: Joint : Joint Motion
+action_scores_2s_jjm = []
+for i in range(len(joint_out_cv)):
+    action_scores_2s_jjm.append(joint_out_cv[i] + jm_out_cv[i])
+
+action_label_2s_jjm = []
+for i in range(len(action_scores_2s_jjm)):
+    action_label_2s_jjm.append(np.argmax(action_scores_2s_jjm[i]))
+
+accuracy_score_2s_jjm = sum(1 for x, y in zip(action_label_2s_jjm, label) if x == y) / float(len(action_label_2s_jjm))
+print("Two stream accuraycy Joint : Joint Motion")
+print(accuracy_score_2s_jjm) 
+
+# Two stream: Joint : Bone Motion
+action_scores_2s_jbm = []
+for i in range(len(joint_out_cv)):
+    action_scores_2s_jbm.append(joint_out_cv[i] + bm_out_cv[i])
+
+action_label_2s_jbm = []
+for i in range(len(action_scores_2s_jbm)):
+    action_label_2s_jbm.append(np.argmax(action_scores_2s_jbm[i]))
+
+accuracy_score_2s_jbm = sum(1 for x, y in zip(action_label_2s_jbm, label) if x == y) / float(len(action_label_2s_jbm))
+print("Two stream accuraycy Joint : Bone Motion")
+print(accuracy_score_2s_jbm)
+
+# Two stream: Bone : Joint Motion
+action_scores_2s_bjm = []
+for i in range(len(joint_out_cv)):
+    action_scores_2s_bjm.append(bone_out_cv[i] + jm_out_cv[i])
+
+action_label_2s_bjm = []
+for i in range(len(action_scores_2s_bjm)):
+    action_label_2s_bjm.append(np.argmax(action_scores_2s_bjm[i]))
+
+accuracy_score_2s_bjm = sum(1 for x, y in zip(action_label_2s_bjm, label) if x == y) / float(len(action_label_2s_bjm))
+print("Two stream accuraycy Bone : Joint Motion")
+print(accuracy_score_2s_bjm)
+
+# Two stream: Bone : Bone Motion
+action_scores_2s_bbm = []
+for i in range(len(joint_out_cv)):
+    action_scores_2s_bbm.append(bone_out_cv[i] + bm_out_cv[i])
+
+action_label_2s_bbm = []
+for i in range(len(action_scores_2s_bbm)):
+    action_label_2s_bbm.append(np.argmax(action_scores_2s_bbm[i]))
+
+accuracy_score_2s_bbm = sum(1 for x, y in zip(action_label_2s_bbm, label) if x == y) / float(len(action_label_2s_bbm))
+print("Two stream accuraycy Bone : Bone Motion")
+print(accuracy_score_2s_bbm)
+
+# Two stream: Joint Motion : Bone Motion
+action_scores_2s_jmbm = []
+for i in range(len(joint_out_cv)):
+    action_scores_2s_jmbm.append(jm_out_cv[i] + bm_out_cv[i])
+
+action_label_2s_jmbm = []
+for i in range(len(action_scores_2s_jmbm)):
+    action_label_2s_jmbm.append(np.argmax(action_scores_2s_jmbm[i]))
+
+accuracy_score_2s_jmbm = sum(1 for x, y in zip(action_label_2s_jmbm, label) if x == y) / float(len(action_label_2s_jmbm))
+print("Two stream accuraycy Joint Motion : Bone Motion")
+print(accuracy_score_2s_jmbm)
+
+### Cross view three stream experiments
+# Three stream: Joint : Bone : Joint Motion
+action_scores_2s_jbjm = []
+for i in range(len(joint_out_cv)):
+    action_scores_2s_jbjm.append(joint_out_cv[i] + bone_out_cv[i] + jm_out_cv[i])
+
+action_label_2s_jbjm = []
+for i in range(len(action_scores_2s_jbjm)):
+    action_label_2s_jbjm.append(np.argmax(action_scores_2s_jbjm[i]))
+
+accuracy_score_2s_jbjm = sum(1 for x, y in zip(action_label_2s_jbjm, label) if x == y) / float(len(action_label_2s_jbjm))
+print("Three stream accuraycy Joint : Bone : Joint Motion")
+print(accuracy_score_2s_jbjm)
+
+# Three stream: Joint : Bone : Bone Motion
+action_scores_2s_jbbm = []
+for i in range(len(joint_out_cv)):
+    action_scores_2s_jbbm.append(joint_out_cv[i] + bone_out_cv[i] + bm_out_cv[i])
+
+action_label_2s_jbbm = []
+for i in range(len(action_scores_2s_jbbm)):
+    action_label_2s_jbbm.append(np.argmax(action_scores_2s_jbbm[i]))
+
+accuracy_score_2s_jbbm = sum(1 for x, y in zip(action_label_2s_jbbm, label) if x == y) / float(len(action_label_2s_jbbm))
+print("Three stream accuraycy Joint : Bone : Bone Motion")
+print(accuracy_score_2s_jbbm)
 
 
+### Cross View Benchmark Four stream experiments
+# Four stream: Joint : Bone : JM : BM = 1 : 1 : 1 : 1
+action_scores_4s = []
+for i in range(len(joint_out_cv)):
+    action_scores_4s.append(joint_out_cv[i] + bone_out_cv[i] + jm_out_cv[i] + bm_out_cv[i])
+
+action_label_4s = []
+for i in range(len(action_scores_4s)):
+    action_label_4s.append(np.argmax(action_scores_4s[i]))
+
+accuracy_score_4s = sum(1 for x, y in zip(action_label_4s, label) if x == y) / float(len(action_label_4s))
+print("Four stream accuraycy Joint : Bone : JM : BM = 1 : 1 : 1 : 1")
+print(accuracy_score_4s)
+
+# Four stream: Joint : Bone : JM : BM = 2 : 1 : 1 : 1
+action_scores_4s_21 = []
+for i in range(len(joint_out_cv)):
+    action_scores_4s_21.append(2 * joint_out_cv[i] + bone_out_cv[i] + jm_out_cv[i] + bm_out_cv[i])
+
+action_label_4s_21 = []
+for i in range(len(action_scores_4s_21)):
+    action_label_4s_21.append(np.argmax(action_scores_4s_21[i]))
+
+accuracy_score_4s_21 = sum(1 for x, y in zip(action_label_4s_21, label) if x == y) / float(len(action_label_4s_21))
+print("Four stream accuraycy Joint : Bone : JM : BM = 2 : 1 : 1 : 1")
+print(accuracy_score_4s_21)
+
+# Four stream: Joint : Bone : JM : BM = 3 : 1 : 1 : 1
+action_scores_4s_31 = []
+for i in range(len(joint_out_cv)):
+    action_scores_4s_31.append(3 * joint_out_cv[i] + bone_out_cv[i] + jm_out_cv[i] + bm_out_cv[i])
+
+action_label_4s_31 = []
+for i in range(len(action_scores_4s_21)):
+    action_label_4s_31.append(np.argmax(action_scores_4s_31[i]))
+
+accuracy_score_4s_31 = sum(1 for x, y in zip(action_label_4s_31, label) if x == y) / float(len(action_label_4s_31))
+print("Four stream accuraycy Joint : Bone : JM : BM = 3 : 1 : 1 : 1")
+print(accuracy_score_4s_31)
+
+# Four stream: Joint : Bone : JM : BM = 4 : 1 : 1 : 1
+# action_scores_4s_41 = []
+# for i in range(len(joint_out_cv)):
+#     action_scores_4s_41.append(4 * joint_out_cv[i] + bone_out_cv[i] + jm_out_cv[i] + bm_out_cv[i])
+
+# action_label_4s_41 = []
+# for i in range(len(action_scores_4s_41)):
+#     action_label_4s_41.append(np.argmax(action_scores_4s_41[i]))
+
+# accuracy_score_4s_41 = sum(1 for x, y in zip(action_label_4s_41, label) if x == y) / float(len(action_label_4s_41))
+# print("Four stream accuraycy Joint : Bone : JM : BM = 4 : 1 : 1 : 1")
+# print(accuracy_score_4s_41)
+
+# Four stream: Joint : Bone : JM : BM = 2 : 2 : 1 : 1
+action_scores_4s_22 = []
+for i in range(len(joint_out_cv)):
+    action_scores_4s_22.append(2 * joint_out_cv[i] + 2 * bone_out_cv[i] + jm_out_cv[i] + bm_out_cv[i])
+
+action_label_4s_22 = []
+for i in range(len(action_scores_4s_22)):
+    action_label_4s_22.append(np.argmax(action_scores_4s_22[i]))
+
+accuracy_score_4s_22 = sum(1 for x, y in zip(action_label_4s_22, label) if x == y) / float(len(action_label_4s_22))
+print("Four stream accuraycy Joint : Bone : JM : BM = 2 : 2 : 1 : 1")
+print(accuracy_score_4s_22)
+
+# Four stream: Joint : Bone : JM : BM = 3 : 2 : 1 : 1
+action_scores_4s_32 = []
+for i in range(len(joint_out_cv)):
+    action_scores_4s_32.append(3 * joint_out_cv[i] + 2 * bone_out_cv[i] + jm_out_cv[i] + bm_out_cv[i])
+
+action_label_4s_32 = []
+for i in range(len(action_scores_4s_32)):
+    action_label_4s_32.append(np.argmax(action_scores_4s_32[i]))
+
+accuracy_score_4s_32 = sum(1 for x, y in zip(action_label_4s_32, label) if x == y) / float(len(action_label_4s_32))
+print("Four stream accuraycy Joint : Bone : JM : BM = 3 : 2 : 1 : 1")
+print(accuracy_score_4s_32)
+
+# Four stream: Joint : Bone : JM : BM = 4 : 2 : 1 : 1
+action_scores_4s_42 = []
+for i in range(len(joint_out_cv)):
+    action_scores_4s_42.append(4 * joint_out_cv[i] + 2 * bone_out_cv[i] + jm_out_cv[i] + bm_out_cv[i])
+
+action_label_4s_42 = []
+for i in range(len(action_scores_4s_42)):
+    action_label_4s_42.append(np.argmax(action_scores_4s_42[i]))
+
+accuracy_score_4s_42 = sum(1 for x, y in zip(action_label_4s_42, label) if x == y) / float(len(action_label_4s_42))
+print("Four stream accuraycy Joint : Bone : JM : BM = 4 : 2 : 1 : 1")
+print(accuracy_score_4s_42)
