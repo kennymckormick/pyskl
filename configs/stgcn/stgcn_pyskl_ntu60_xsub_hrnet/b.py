1dc8b82f2d5ebee805d816a96fb8430fd4db0d7d
anhvu2501 <vu25ck28@gmail.com>
2023-05-21 14:12:21 +0700

    add drop out to prevent overfitting

--- configs/stgcn/stgcn_pyskl_ntu60_xsub_hrnet/b.py
+++ configs/stgcn/stgcn_pyskl_ntu60_xsub_hrnet/b.py
@@ -50,7 +50,7 @@ optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0005, nesterov
 optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(policy='CosineAnnealing', min_lr=0, by_epoch=False)
-total_epochs = 16
+total_epochs = 14
 checkpoint_config = dict(interval=1)
 evaluation = dict(interval=1, metrics=['top_k_accuracy'])
 log_config = dict(interval=100, hooks=[dict(type='TextLoggerHook')])
